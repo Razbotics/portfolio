f9ae9e7b21b97bd4dd4d53304f7526854d95b71f
Shubhankar <das.shubhankar97@gmail.com>
2020-12-04 21:50:57 +0530

    ci changes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo 'Starting to Deploy...'
-ssh ubuntu@3.138.222.105 "docker container rm portfolio -f
-        docker pull razbotics/portfolio:latest
-        docker run --name portfolio -d -p 3000:80 razbotics/portfolio:latest
-        "
+ssh server "
+            docker container rm portfolio -f
+            docker pull razbotics/portfolio:latest
+            docker run --name portfolio -d -p 3000:80 razbotics/portfolio:latest
+            "
 echo 'Deployment completed successfully'
