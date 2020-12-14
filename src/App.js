d8646ee939078b77c88c736736bfeb7b0c9b0199
Shubhankar <das.shubhankar97@gmail.com>
2020-12-14 21:21:17 +0530

    added contacts page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutMePage from "./pages/AboutMePage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
     <Router>
+      <Route path="/contact" component={ContactPage} />
       <Route path="/about-me" component={AboutMePage} />
       <Route exact path="/" component={HomePage} />
     </Router>
